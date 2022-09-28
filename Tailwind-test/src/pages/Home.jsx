1c4840119fb640bd0d65f4b7870dc2876df6bbbf
Demian <sodaoo@qq.com>
2022-09-28 23:10:46 +0800

    update..

---
+++ Tailwind-test/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Header from '../partials/Header';
+import HeroHome from '../partials/HeroHome';
+import FeaturesHome from '../partials/Features';
+import FeaturesBlocks from '../partials/FeaturesBlocks';
+import Testimonials from '../partials/Testimonials';
+import Newsletter from '../partials/Newsletter';
+import Footer from '../partials/Footer';
+
+function Home() {
+  return (
+    <div className="bg-[url('../../1.png')]">
+      1111111111111111111111
+    </div>
+  );
+}
+
+export default Home;
